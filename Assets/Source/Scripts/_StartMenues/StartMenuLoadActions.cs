8d26379a864c30a2eec66d326d89357582b41b29
PavelOotsutsuki <109985069+PavelOotsutsuki@users.noreply.github.com>
2026-01-14 00:03:59 +0300

    делаю StartMenu

--- Assets/Source/Scripts/_StartMenues/StartMenuLoadActions.cs
+++ Assets/Source/Scripts/_StartMenues/StartMenuLoadActions.cs
@@ -13,9 +13,14 @@ namespace StartMenues
         [SerializeField] private Color _startColor;
         [SerializeField] private Color _endColor;
         [SerializeField] private StoneSpawner _stoneSpawner;
+        [SerializeField] private LoadText _loadText;
+        [SerializeField] private StartMenu _startMenu;
 
         public void Activate()
         {
+            _loadText.Init();
+            _startMenu.Init();
+
             StartCoroutine(Activating());
 
             _stoneSpawner.Init();
@@ -34,9 +39,16 @@ namespace StartMenues
 
             // 2. Камнепад
 
+            _loadText.Activate();
             _stoneSpawner.Activate();
 
             yield return new WaitUntil(() => _stoneSpawner.IsComplete);
+
+            _loadText.Deactivate();
+
+            // 3. Появление меню
+
+            _startMenu.Activate();
         }
     }
 }
